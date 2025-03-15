031ce95ac3ce8127cc27d58aa64d1b78c0d1625b
codeno1r <gvarrworks@gmail.com>
2025-03-15 22:05:35 +0800

    added more blocks

--- blocks.h
+++ blocks.h
@@ -1,9 +1,11 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", 		"volume.sh",		0,			10},
+	{"󰖂 ", 		"nordlayer status | grep VPN | sed s/'VPN: '//",		15,			0},
 	{" ", 		"top -bn1 | grep load | awk '/^top/ {print $11}' | sed s/,/%/",		3,			18},
 	{" ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+	{"", 		"volume.sh",		0,			10},
+	{"", 		"battery.sh",		5,			3},
 	{"", "date '+%b %d (%a) %I:%M%p'",					1,		0},
 };
 
